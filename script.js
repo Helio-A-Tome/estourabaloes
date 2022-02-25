fc86b3820225b25f36c44fd007ea2e66db40ea5e
Helio-A-Tome <netsharkinformatica@gmail.com>
2022-02-25 07:35:45 -0300

    mudando de cor aleatoriamente

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function adiBola() {
 
+    var box = document.getElementsByClassName('bola');
+    var background = ["#f36", "#C3C", "#fc0", "#FC6", "#9C0", "#363636", "#000", "#000080", "#006400", "#8B4513", "#4B0082"];
+    cor = 0;
+    div = 0;
+    setInterval(function() {
+        box.item(div).style.backgroundColor = background[cor];
+        cor++;
+        div++;
+        if (cor >= background.length) {
+            cor = 0;
+        }
+        if (div >= box.length) {
+            div = 0;
+        }
+    }, 1000)
+
+
     var bola = document.createElement("div");
     bola.setAttribute("class", "bola");
 
